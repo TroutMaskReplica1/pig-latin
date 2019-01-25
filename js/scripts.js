c168e52ddea2c64912a57ba77c8c3dcb6d0126fb
Nelia Terrazas and Ryan Dikeos <nelia@>
2019-01-24 16:14:52 -0800

    add functions, get things working

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
 //Business Logic
 var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"];
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "qu", "r", "s", "t", "v", "w", "x", "y", "z"];
 
-function addWay(word) {
-  if (vowels.includes(word[0])) {
-  return word + "way";
+function vowel(word) {
+  if (vowels.includes(word[0])) { //if it starts with a vowel
+    return true;
+  } else {
+    return false;
   }
 }
 
-function addAy(word) {
-  if (consonants.includes(word[0])) {
-      return word + "ay"
+function cons(word) {
+  if (!vowels.includes(word[0])) {
+  return alert("Cons")
+}
+}
+
+function pigLatin(word) {
+  for (i = 0; i < word.length; i++) {
+    if (vowel(word) === true) { //if it starts with a vowel
+      return word + "way";
+    } else if (vowel(word) === false) { //if it starts with a consonant
+      return word + "ay";
+    }
   }
 }
+
 
 $(document).ready(function() {
-  $("form#form1").submit(function() {
+  $("form#form1").submit(function(event) {
     event.preventDefault();
     var input = $("input#phrase").val();
-    var output = addWay(input);
+    var output = pigLatin(input);
     $("#result").show();
+    $("#result").text(output);
   });
 });
